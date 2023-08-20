f0a5317f5d04df5d50c15119c574ecf221c5c87d
jbryan83 <james.bryan.home@gmail.com>
2023-08-20 14:26:07 -0500

    Reduce edge function size

--- app/share/[id]/page.tsx
+++ app/share/[id]/page.tsx
@@ -34,12 +34,12 @@ export default async function SharePage({ params }: SharePageProps) {
 
   return (
     <>
-      <div className="flex-1 space-y-6">
-        <div className="border-b bg-background px-4 py-6 md:px-6 md:py-8">
-          <div className="mx-auto max-w-2xl md:px-6">
-            <div className="space-y-1 md:-mx-8">
-              <h1 className="text-2xl font-bold">{chat.title}</h1>
-              <div className="text-sm text-muted-foreground">
+      <div className='flex-1 space-y-6'>
+        <div className='border-b bg-background px-4 py-6 md:px-6 md:py-8'>
+          <div className='mx-auto max-w-2xl md:px-6'>
+            <div className='space-y-1 md:-mx-8'>
+              <h1 className='text-2xl font-bold'>{chat.title}</h1>
+              <div className='text-sm text-muted-foreground'>
                 {formatDate(chat.createdAt)} · {chat.messages.length} messages
               </div>
             </div>
@@ -47,7 +47,7 @@ export default async function SharePage({ params }: SharePageProps) {
         </div>
         <ChatList messages={chat.messages} />
       </div>
-      <FooterText className="py-8" />
+      <FooterText className='py-8' />
     </>
   )
 }
